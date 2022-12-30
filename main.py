a30e0b69d6d8728c46bd0cd6cc2dbd182d23fb78
brueggemann4 <brueggemann4@uni-potsdam.de>
2022-12-30 20:26:18 +0100

    add finetuning

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 from preprocessing import process, undersample
 from save_result import save
-from vars import features, models, vc
+from vars import *
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 titanic = pd.read_csv("train.csv")
 titanic = process(titanic)
 
 
-def do(data,
-       ens: bool = False,
-       comp: bool = False,
-       save_result: bool = False):
-
+def do(data, job: str = "sr"):
     X = data[features]
     y = data["Survived"]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-    if ens:
+    if job == "ens":
         _ensemble(X_train, X_test, y_train, y_test)
 
-    if comp:
+    if job == "comp":
         _compare(X_train, X_test, y_train, y_test)
 
-    if save_result:
+    if job == "tune":
+        _tune(X_train, y_train)
+
+    if job == "sr":
         _solute(X, y)
 
 
 def _compare(X_train, X_test, y_train, y_test):
-
     for name, model in models.items():
         print("-" * 32 + " " + name + " " + "-" * 32)
 
@@ -48,7 +48,6 @@ def _compare(X_train, X_test, y_train, y_test):
 
 
 def _ensemble(X_train, X_test, y_train, y_test):
-
     vc.fit(X_train, y_train)
     y_pred = vc.predict(X_test)
 
@@ -61,13 +60,22 @@ def _solute(X, y):
     save(model)
 
 
-def charts(data):
+def _tune(X_train, y_train):
+    test = RandomForestClassifier()
+    GRID = tuner["rfc"]
 
+    model = GridSearchCV(test, param_grid=GRID, verbose=2, n_jobs=1)
+    model.fit(X_train, y_train)
+    print(model.score(X_train, y_train))
+    print(model.best_params_)
+
+
+def charts(data):
     for label in features:
-        # print(data[label].nunique())
-        # print(set(data[label]))
-        # data = undersample(data, label)
-        ax = sns.countplot(y=data[label])#[data["Survived"] == 1])
+        print(data[label].nunique())
+        print(set(data[label]))
+        data = undersample(data, label)
+        ax = sns.countplot(y=data[label][data["Survived"] == 0])
         plt.bar_label(ax.containers[0])
         plt.title(label, color="red")
         plt.show()
@@ -75,4 +83,4 @@ def charts(data):
 
 if __name__ == "__main__":
     # charts(titanic)
-    do(titanic, comp=True)
+    do(titanic, job="tune")
